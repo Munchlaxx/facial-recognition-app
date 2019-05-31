95c65ab9ee9e5c1e939e62094ef6f892caf88017
Munchlaxx <evan.mirschel@gmail.com>
2019-05-30 21:37:28 -0400

    Added Components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Navigation from './components/navigation/Navigation'
+import Logo from './components/logo/Logo'
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
+import Rank from './components/Rank/Rank'
+import Particles from 'react-particles-js';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 255,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+}
+
+class App extends Component {
+  render() {
+    return(
+        <div className='App'>
+          <Particles className='particles'
+          params={particlesOptions}
+          />
+          <Navigation />
+          <Logo />
+          <Rank />
+          <ImageLinkForm />
+          {/*<FaceRecognition />*/}
+        </div>
+      );
+  }
 }
 
 export default App;
