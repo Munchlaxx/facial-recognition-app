a29a7b240daf0bafd9b66dbed949b61535298438
Munchlaxx <evan.mirschel@gmail.com>
2019-06-05 16:36:19 -0400

    added api files

---
+++ facial-recognition-app-api/server.js
@@ -0,0 +1,105 @@
+const express = require('express');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+app.use(bodyParser.json())
+app.use(cors());
+
+const database = {
+	users: [
+		{
+			id: '123',
+			name: 'John',
+			email: 'john@gmail.com',
+			password: 'cookies',
+			entries: 0,
+			joined: new Date()
+		},
+		{
+			id: '124',
+			name: 'Sally',
+			email: 'sally@gmail.com',
+			password: 'bananas',
+			entries: 0,
+			joined: new Date()
+		}
+	]
+}
+
+app.get('/', (req, res) => {
+	res.send(database.users);
+})
+
+app.post('/signin', (req,res) => {
+	if(req.body.email === database.users[0].email &&
+		req.body.password === database.users[0].password){
+		res.json('success');
+	}else{
+		res.status(400).json('error logging in');
+	}
+})
+
+app.post('/register', (req,res) => {
+	const { email, name, password } = req.body;
+	database.users.push({
+			id: '125',
+			name: name,
+			email: email,
+			password: password,
+			entries: 0,
+			joined: new Date()
+		})
+
+	res.json(database.users[database.users.length - 1])
+})
+
+app.get('/profile/:id', (req,res) => {
+	const { id } = req.params;
+	let found = false;
+	database.users.forEach(user => {
+		if(user.id === id){
+			found = true;
+			return res.json(user)
+		} 
+	})
+	if(!found){
+		res.status(400).json('user not found');
+	}
+})
+
+app.put('/image', (req, res) => {
+	const { id } = req.body;
+	let found = false;
+	database.users.forEach(user => {
+		if(user.id === id){
+			found = true;
+			user.entries++;
+			return res.json(user.entries)
+		} 
+	})
+	if(!found){
+		res.status(400).json('user not found');
+	}
+})
+
+
+// bcrypt.hash(password, saltRounds).then(function(hash) {
+// 	console.log(hash);
+// });
+// // Load hash from your password DB.
+// bcrypt.compare(myPlaintextPassword, hash).then(function(res) {
+//     // res == true
+// });
+
+// bcrypt.compare(someOtherPlaintextPassword, hash).then(function(res) {
+//     // res == false
+// });
+
+app.listen(3000, () => {
+	console.log('app is running on port 3000');
+})
+
--- src/App.js
+++ src/App.js
@@ -39,6 +39,12 @@ class App extends Component {
     }
   }
 
+  componentDidMount() {
+    fetch('http://localhost:3000/')
+      .then(response => response.json())
+      .then(console.log)
+  }
+
   calculateFaceLocation = (data) => {
     console.log(data);
     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
